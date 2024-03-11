c68a08d4f293ea844a89d6559dc486f4fe7b4485
welcometomygame <160855284+welcometomygame@users.noreply.github.com>
2024-03-11 10:57:44 +0000

    Update main.blocks, main.ts, images.g.jres, images.g.ts, tilemap.g.jres, tilemap.g.ts, main.py

--- main.py
+++ main.py
@@ -1,163 +1,118 @@
 @namespace
 class SpriteKind:
     energy = SpriteKind.create()
-
-def on_on_overlap(sprite, otherSprite):
-    info.change_score_by(1)
-    sprites.destroy(otherSprite)
-sprites.on_overlap(SpriteKind.player, SpriteKind.energy, on_on_overlap)
-
-def on_a_pressed():
-    if RED_TEAM.vy == 0:
-        RED_TEAM.vy = -150
-controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+    Skull = SpriteKind.create()
+    Ball = SpriteKind.create()
+    Goalie = SpriteKind.create()
+    DEF = SpriteKind.create()
+    CAP = SpriteKind.create()
 
 def on_overlap_tile(sprite2, location):
     game.game_over(False)
-    game.set_game_over_effect(False, effects.bubbles)
-scene.on_overlap_tile(SpriteKind.player,
+    game.set_game_over_effect(False, effects.dissolve)
+    game.set_game_over_message(False, "OWN GOAL!")
+scene.on_overlap_tile(SpriteKind.Ball,
     assets.tile("""
-        myTile2
+        myTile17
     """),
     on_overlap_tile)
 
-def on_overlap_tile2(sprite3, location2):
+def on_a_pressed():
+    if RED_TEAM.vy == 100:
+        RED_TEAM.vy = -250
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
+
+def on_overlap_tile2(sprite22, location2):
     game.game_over(True)
-scene.on_overlap_tile(SpriteKind.player,
+    game.set_game_over_effect(True, effects.confetti)
+    game.set_game_over_message(True, "GOAL!")
+scene.on_overlap_tile(SpriteKind.Ball,
     assets.tile("""
-        myTile5
+        myTile1
     """),
     on_overlap_tile2)
 
-Energy: Sprite = None
 RED_TEAM: Sprite = None
-scene.set_background_color(6)
+info.start_countdown(12)
+scene.set_background_color(7)
 RED_TEAM = sprites.create(assets.image("""
         Red Team character
     """),
     SpriteKind.player)
-controller.move_sprite(RED_TEAM, 100, 0)
+Ball2 = sprites.create(assets.image("""
+    Ball
+"""), SpriteKind.Ball)
+Ball2.set_position(88, 70)
+controller.move_sprite(RED_TEAM, 105, 105)
 tiles.set_current_tilemap(tilemap("""
     level1 map arena
 """))
-RED_TEAM.ay = 200
 scene.camera_follow_sprite(RED_TEAM)
-for value in tiles.get_tiles_by_type(assets.tile("""
-    myTile7
-""")):
-    Energy = sprites.create(img("""
+GK = sprites.create(assets.image("""
+    Goalie
+"""), SpriteKind.Goalie)
+GK.follow(RED_TEAM, 70)
+GK.set_position(260, 55)
+DEF2 = sprites.create(assets.image("""
+    DEF
+"""), SpriteKind.DEF)
+DEF2.follow(RED_TEAM, 80)
+DEF2.set_position(760, 120)
+CAP2 = sprites.create(assets.image("""
+    CAP
+"""), SpriteKind.CAP)
+CAP2.follow(RED_TEAM, 60)
+CAP2.set_position(500, 100)
+MID2 = sprites.create(assets.image("""
+    CAP
+"""), SpriteKind.CAP)
+MID2.follow(RED_TEAM, 30)
+MID2.set_position(400, 200)
+mySprite = sprites.create(img("""
+        . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . c c c . . . . . . . . 
-                    . . . . 2 2 2 2 2 . . . . . . . 
-                    . . . 2 2 2 2 2 2 2 . . . . . . 
-                    . . . 2 2 2 2 2 2 2 . . . . . . 
-                    . . . 5 5 5 5 5 5 5 . . . . . . 
-                    . . . 2 2 2 2 2 2 2 . . . . . . 
-                    . . . 5 5 5 5 5 5 5 . . . . . . 
-                    . . . 2 2 2 2 2 2 2 . . . . . . 
-                    . . . 2 2 2 2 2 2 2 . . . . . . 
-                    . . . 2 2 2 2 2 2 2 . . . . . . 
-                    . . . 2 2 2 2 2 2 2 . . . . . . 
-                    . . . . . . . . . . . . . . . . 
-                    . . . . . . . . . . . . . . . .
-        """),
-        SpriteKind.energy)
-    animation.run_image_animation(Energy,
-        [img("""
-                . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . f f f . . . . . . . . 
-                        . . . . 2 2 2 2 2 . . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 5 5 5 5 5 5 5 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 5 5 5 5 5 5 5 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . .
-            """),
-            img("""
-                . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . f . . . . . . . . . 
-                        . . . . . . 2 . . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 5 5 5 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 5 5 5 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . .
-            """),
-            img("""
-                . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . f . . . . . . . . . 
-                        . . . . . . 2 . . . . . . . . . 
-                        . . . . . . 2 . . . . . . . . . 
-                        . . . . . . 2 . . . . . . . . . 
-                        . . . . . . 5 . . . . . . . . . 
-                        . . . . . . 2 . . . . . . . . . 
-                        . . . . . . 5 . . . . . . . . . 
-                        . . . . . . 2 . . . . . . . . . 
-                        . . . . . . 2 . . . . . . . . . 
-                        . . . . . . 2 . . . . . . . . . 
-                        . . . . . . 2 . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . .
-            """),
-            img("""
-                . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . f . . . . . . . . . 
-                        . . . . . . 2 . . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 5 5 5 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 5 5 5 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . 2 2 2 . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . .
-            """),
-            img("""
-                . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . f f f . . . . . . . . 
-                        . . . . 2 2 2 2 2 . . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 5 5 5 5 5 5 5 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 5 5 5 5 5 5 5 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . 2 2 2 2 2 2 2 . . . . . . 
-                        . . . . . . . . . . . . . . . . 
-                        . . . . . . . . . . . . . . . .
-            """)],
-        100,
-        True)
-    tiles.place_on_tile(Energy, value)
-    tiles.set_tile_at(value, assets.tile("""
-        transparency16
-    """))
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . .
+    """),
+    SpriteKind.player)
+
+def on_forever():
+    if controller.B.is_pressed():
+        if RED_TEAM.overlaps_with(Ball2):
+            controller.move_sprite(Ball2, 105, 105)
+forever(on_forever)
+
+def on_forever2():
+    if controller.A.is_pressed():
+        controller.move_sprite(Ball2, 200, 200)
+forever(on_forever2)
+
+def on_forever3():
+    if GK.overlaps_with(RED_TEAM):
+        sprites.destroy(RED_TEAM)
+        game.game_over(False)
+forever(on_forever3)
+
+def on_forever4():
+    if CAP2.overlaps_with(RED_TEAM):
+        sprites.destroy(RED_TEAM)
+        game.game_over(False)
+forever(on_forever4)
+
+def on_forever5():
+    if DEF2.overlaps_with(RED_TEAM):
+        sprites.destroy(RED_TEAM)
+        game.game_over(False)
+forever(on_forever5)
--- main.ts
+++ main.ts
@@ -1,140 +1,121 @@
 namespace SpriteKind {
     export const energy = SpriteKind.create()
+    export const Skull = SpriteKind.create()
+    export const Ball = SpriteKind.create()
+    export const Goalie = SpriteKind.create()
+    export const DEF = SpriteKind.create()
+    export const CAP = SpriteKind.create()
+    export const MAN2 = SpriteKind.create()
 }
-sprites.onOverlap(SpriteKind.Player, SpriteKind.energy, function (sprite, otherSprite) {
-    info.changeScoreBy(1)
-    sprites.destroy(otherSprite)
+scene.onOverlapTile(SpriteKind.Ball, assets.tile`myTile17`, function (sprite2, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.dissolve)
+    game.setGameOverMessage(false, "OWN GOAL!")
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (RED_TEAM.vy == 0) {
-        RED_TEAM.vy = -150
+    if (RED_TEAM.vy == 100) {
+        RED_TEAM.vy = -250
     }
 })
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile2`, function (sprite, location) {
-    game.gameOver(false)
-    game.setGameOverEffect(false, effects.bubbles)
-})
-scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile5`, function (sprite, location) {
+scene.onOverlapTile(SpriteKind.Ball, assets.tile`myTile1`, function (sprite2, location) {
     game.gameOver(true)
+    game.setGameOverEffect(true, effects.confetti)
+    game.setGameOverMessage(true, "GOAL!")
 })
-let Energy: Sprite = null
 let RED_TEAM: Sprite = null
-scene.setBackgroundColor(6)
+info.startCountdown(13)
+scene.setBackgroundColor(7)
 RED_TEAM = sprites.create(assets.image`Red Team character`, SpriteKind.Player)
-controller.moveSprite(RED_TEAM, 100, 0)
+let Ball2 = sprites.create(assets.image`Ball`, SpriteKind.Ball)
+Ball2.setPosition(95, 85)
+controller.moveSprite(RED_TEAM, 105, 105)
+RED_TEAM.setPosition(88, 75)
 tiles.setCurrentTilemap(tilemap`level1 map arena`)
-RED_TEAM.ay = 200
 scene.cameraFollowSprite(RED_TEAM)
-for (let value of tiles.getTilesByType(assets.tile`myTile7`)) {
-    Energy = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . c c c . . . . . . . . 
-        . . . . 2 2 2 2 2 . . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 5 5 5 5 5 5 5 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 5 5 5 5 5 5 5 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, SpriteKind.energy)
-    animation.runImageAnimation(
-    Energy,
-    [img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . f f f . . . . . . . . 
-        . . . . 2 2 2 2 2 . . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 5 5 5 5 5 5 5 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 5 5 5 5 5 5 5 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . f . . . . . . . . . 
-        . . . . . . 2 . . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 5 5 5 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 5 5 5 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . f . . . . . . . . . 
-        . . . . . . 2 . . . . . . . . . 
-        . . . . . . 2 . . . . . . . . . 
-        . . . . . . 2 . . . . . . . . . 
-        . . . . . . 5 . . . . . . . . . 
-        . . . . . . 2 . . . . . . . . . 
-        . . . . . . 5 . . . . . . . . . 
-        . . . . . . 2 . . . . . . . . . 
-        . . . . . . 2 . . . . . . . . . 
-        . . . . . . 2 . . . . . . . . . 
-        . . . . . . 2 . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . f . . . . . . . . . 
-        . . . . . . 2 . . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 5 5 5 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 5 5 5 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . 2 2 2 . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `,img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . f f f . . . . . . . . 
-        . . . . 2 2 2 2 2 . . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 5 5 5 5 5 5 5 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 5 5 5 5 5 5 5 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . 2 2 2 2 2 2 2 . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `],
-    100,
-    true
-    )
-    tiles.placeOnTile(Energy, value)
-    tiles.setTileAt(value, assets.tile`transparency16`)
-}
+let GK = sprites.create(assets.image`Goalie`, SpriteKind.Goalie)
+GK.follow(RED_TEAM, 70)
+GK.setPosition(260, 55)
+let DEF2 = sprites.create(assets.image`DEF`, SpriteKind.DEF)
+DEF2.follow(RED_TEAM, 80)
+DEF2.setPosition(760, 120)
+let CAP2 = sprites.create(assets.image`man`, SpriteKind.CAP)
+CAP2.follow(RED_TEAM, 60)
+CAP2.setPosition(500, 100)
+let MID2 = sprites.create(assets.image`man`, SpriteKind.CAP)
+MID2.follow(RED_TEAM, 30)
+MID2.setPosition(400, 200)
+let DEF_3 = sprites.create(assets.image`man`, SpriteKind.CAP)
+DEF_3.follow(RED_TEAM, 80)
+DEF_3.setPosition(200, 200)
+let GK2 = sprites.create(img`
+    ........................
+    ..........fffff.........
+    ..........fdddf.........
+    ....eeee..dfdfd..eeee...
+    ....eeee..ddddd..eeee...
+    .....ee...ddddd...ee....
+    .....dd...11111...dd....
+    .....dd...7f7f7...dd....
+    .....ddd7f7f7f7f7ddd....
+    .....ddd7f7f7f7f7ddd....
+    ..........7f7f7.........
+    ..........7f7f7.........
+    ..........7f7f7.........
+    ..........77777.........
+    ..........fffff.........
+    ..........fffff.........
+    ..........fffff.........
+    ..........ff.ff.........
+    ..........ff.ff.........
+    ..........dd.dd.........
+    ..........dd.dd.........
+    ..........77.77.........
+    ..........77.77.........
+    ..........77.77.........
+    .........fff.fff........
+    .........fff.fff........
+    `, SpriteKind.CAP)
+GK2.follow(RED_TEAM, 20)
+GK2.setPosition(900, 150)
+forever(function () {
+    if (controller.B.isPressed()) {
+        if (RED_TEAM.overlapsWith(Ball2)) {
+            controller.moveSprite(Ball2, 105, 105)
+        }
+    }
+})
+forever(function () {
+    if (controller.A.isPressed()) {
+        controller.moveSprite(Ball2, 200, 200)
+    }
+})
+forever(function () {
+    if (GK.overlapsWith(RED_TEAM)) {
+        sprites.destroy(RED_TEAM)
+        game.gameOver(false)
+    }
+})
+forever(function () {
+    if (DEF_3.overlapsWith(RED_TEAM)) {
+        sprites.destroy(RED_TEAM)
+        game.gameOver(false)
+    }
+})
+forever(function () {
+    if (CAP2.overlapsWith(RED_TEAM)) {
+        sprites.destroy(RED_TEAM)
+        game.gameOver(false)
+    }
+})
+forever(function () {
+    if (DEF2.overlapsWith(RED_TEAM)) {
+        sprites.destroy(RED_TEAM)
+        game.gameOver(false)
+    }
+})
+forever(function () {
+    if (GK2.overlapsWith(Ball2)) {
+        sprites.destroy(Ball2)
+        game.gameOver(false)
+    }
+})
